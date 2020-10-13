a0f4098d34af58ab670d3e8143c7fffec8076421
Benjamin Hazlett <bhazlett@debian.BensVM>
2020-10-13 17:14:58 -0400

    Chapter 1

---
+++ Chapter_1/hello_cargo/target/debug/deps/hello_cargo-d5b6432f8d84d54f.d
@@ -0,0 +1,5 @@
+/home/bhazlett/Projects/Rust/Rust_The_Book/Chapter_1/hello_cargo/target/debug/deps/hello_cargo-d5b6432f8d84d54f: src/main.rs
+
+/home/bhazlett/Projects/Rust/Rust_The_Book/Chapter_1/hello_cargo/target/debug/deps/hello_cargo-d5b6432f8d84d54f.d: src/main.rs
+
+src/main.rs:
---
+++ Chapter_1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/bhazlett/Projects/Rust/Rust_The_Book/Chapter_1/hello_cargo/target/debug/hello_cargo: /home/bhazlett/Projects/Rust/Rust_The_Book/Chapter_1/hello_cargo/src/main.rs
